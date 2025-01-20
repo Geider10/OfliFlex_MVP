ff16583a1039ab0986745174519644d1a6969a35
Geider Frias <geiderde20@gmail.com>
2025-01-19 22:52:17 -0300

    convert footer in responsive

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from './Footer.module.css'
-import { Link, NavLink } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 import { PiInstagramLogoLight } from "react-icons/pi";
 import { SlSocialFacebook } from "react-icons/sl";
 import { RiTwitterXLine } from "react-icons/ri";
@@ -9,7 +9,6 @@ const Footer = () => {
     <div className={styles.container}>
       <Link className={styles.container_logo} to={'/'}><img src="/footer_logotipo.webp" alt="" className={styles.logo} /></Link>
       <ul className={styles.div}>
-        {/* <NavLink className={styles.links} to={'/'}>Inicio</NavLink> */}
         <Link className={styles.links} to={'/'}>Inicio</Link>
         <Link className={styles.links} to={'/servicios/oficinas'}>Servicios</Link>
         <Link className={styles.links} to={'/galeria'}>Galeria</Link>
