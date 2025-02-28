0d4e468f2ea858ea6df18fda305077541cdab0a1
Geider Frias <geiderde20@gmail.com>
2025-02-28 01:02:36 -0300

    updated file config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,11 +1,11 @@
-//variables de desarrollo
+//ambite de produccion o de desarrollo
 const PORT = process.env.PORT || '3000'
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://Yoel10:yoel2024@dbtesting.lrttm0z.mongodb.net/Ofiflex?retryWrites=true&w=majority&appName=DBTesting'
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'la_contraseña_mas_segura_y_fiable_27/10/2024'
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '233276774937-dlpho3it1g2oq9kbefe5u7grk9te3sth.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET=  process.env.GOOGLE_CLIENT_SECRET || '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'
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
-const EMAIL_ADMIN = process.env.EMAIL_ADMIN || 'd0m1de24@gmail.com'
-const PASSWORD_ADMIN = process.env.PASSWORD_ADMIN || 'abpb kbtf ffdl nqvr'
+const MONGO_URI = process.env.MONGO_URI || process.env.MONGO_URI_DEV
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || process.env.JWT_SECRET_KEY_DEV
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || process.env.GOOGLE_CLIENT_ID_DEV
+const GOOGLE_CLIENT_SECRET=  process.env.GOOGLE_CLIENT_SECRET || process.env.GOOGLE_CLIENT_SECRET_DEV
+const FRONTEND_URL = process.env.FRONTEND_URL || process.env.FRONTEND_URL_DEV
+const EMAIL_ADMIN = process.env.EMAIL_ADMIN || process.env.EMAIL_ADMIN_DEV
+const PASSWORD_ADMIN = process.env.PASSWORD_ADMIN || process.env.PASSWORD_ADMIN_DEV
 
 module.exports = {PORT, MONGO_URI, JWT_SECRET_KEY, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, FRONTEND_URL, EMAIL_ADMIN, PASSWORD_ADMIN}
